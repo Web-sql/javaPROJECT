c72135eb249b6ddaa6e7c9c70edaf610c6deea62
Web-sql <kamalprasad09711@gmail.com>
2023-11-02 23:33:29 +0530

    first

---
+++ java43.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class java43 {
+    public static void main(String arg[]) {
+        int n, sum = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter how many numbers you want to sum:");
+        n = sc.nextInt();
+        int a[] = new int[n];
+        System.out.println("Enter the " + n + " numbers:");
+        for (int i = 0; i < n; i++) {
+            System.out.println("Enter number " + (i + 1) + ":");
+            a[i] = sc.nextInt();
+        }
+        for (int i = 0; i < n; i++) {
+            sum += a[i];
+        }
+        System.out.println("Sum of " + n + " numbers is =" + sum);
+    }
+}
+    
+
